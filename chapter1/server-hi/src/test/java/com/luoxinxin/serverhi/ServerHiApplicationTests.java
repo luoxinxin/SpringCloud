4ef174de36a9d9d353fa248c9aa3496c7c98dd8c
luoxinxin <2538971540@qq.com>
2019-12-13 09:26:23 +0800

    springcloud chapter1

---
+++ chapter1/server-hi/src/test/java/com/luoxinxin/serverhi/ServerHiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luoxinxin.serverhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerHiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
